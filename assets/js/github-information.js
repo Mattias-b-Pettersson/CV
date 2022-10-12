400ebb73dd347d9f2d478c1eb4211c426939c029
Mattias-b-Pettersson <mattep3@gmail.com>
2022-10-12 20:58:19 +0000

    Add github API display function

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,20 @@
+function userInformationHTML(user) {
+    return `
+    <h2>${user.name}
+        <span class="small-name">
+            (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+        </span>
+    </h2>
+    <div class="gh-content">
+        <div class="gh-avatar">
+            <a href="${user.html_url} target="_blank">
+                <img src="${user.avatar_url}" width="80px" height="80px" alt="${user.login}">
+            </a>
+        </div>
+        <p>Följare: ${user.followers} - Följer: ${user.following} <br> Repos: ${user.public_repos}</p>
+    </div>`
+}
+
 function fetchGithubInformation(event) {
 
     var username = $("#gh-username").val()
