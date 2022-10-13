6e3f13688de5023f6544a0547b19ecdcb427249e
Mattias-b-Pettersson <mattep3@gmail.com>
2022-10-13 18:52:57 +0000

    Fix bugs for the gh-user-data and gh-repo-data div

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGithubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val()
     if (!username) {
