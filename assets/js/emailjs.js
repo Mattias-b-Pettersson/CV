c27603944421ef872889aece0b9c1fbaa16439fe
Mattias-b-Pettersson <mattep3@gmail.com>
2022-10-15 19:09:15 +0000

    Add sendMail function

--- assets/js/emailjs.js
+++ assets/js/emailjs.js
@@ -1,3 +1,26 @@
+function sendMail(contactForm) {
+    console.log("Försöker skicka mail...")
+
+    fromName = document.getElementById("name").value;
+    message = document.getElementById("projectsummary").value;
+    replyTo = document.getElementById("email").value;
+
+    emailjs.send("service_c07g64h", "CV", {
+        from_name: fromName,
+        message: message,
+        reply_to: replyTo
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response)
+        },
+        function(error) {
+            console.log("FAILED", error)
+        }
+    )
+    return false;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("form-submit").addEventListener("click", function() {
         sendMail(this);
